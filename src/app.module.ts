ac5dc2e8bb72f25aaa902a4bfd84920b35503ea2
Mai Vũ Cao Sơn <maivucaoson2004@gmail.com>
2025-03-24 01:34:36 +0700

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
+import { JwtStrategy } from '@/auth/guards/strategies/jwt.strategy'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
-import { JwtStrategy } from '@/auth/guards/strategies/jwt.strategy'
-import { UsersModule } from './users/users.module'
-import { APP_GUARD } from '@nestjs/core'
-import { RolesGuard } from '@/auth/guards/roles/roles.guard'
-import { JwtAuthGuard } from '@/auth/guards/ jwt-auth.guard'
-import { ProductsModule } from './products/products.module'
 import { CartModule } from './cart/cart.module'
-import { ChatGateway } from './gateway/chat.gateway'
 import { FriendsModule } from './friends/friends.module'
+import { ChatGateway } from './gateway/chat.gateway'
+import { ProductsModule } from './products/products.module'
+import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as cookieParser from 'cookie-parser'
 import * as express from 'express'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
